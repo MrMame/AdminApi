130ef299cfd28e866f48549a21dd946593bdf4f0
MrMame <hereAtLaptop@github.com>
2025-12-29 12:53:39 +0100

    - Changed .env files location to backend/config folder

--- projects/backend/config/env.js
+++ projects/backend/config/env.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
 import fs from "fs";
 
-const env = process.env.NODE_ENV || "dev";
-const envFile = `.env.${env}`;
+const env               = process.env.NODE_ENV || "dev";
+const envFile           = `config/.env.${env}`;
+const envFallbackFile   = `config/.env`;
 
 if(fs.existsSync(envFile)){
+    console.log(`Going to load .env file '${envFile}'`);
     dotenv.config({path:envFile});
 }else{
-    dotenv.config();        // fallback is .env
+    console.log(`Trying to load .env file '${envFallbackFile}'`);
+    dotenv.config({path:envFallbackFile});        // fallback is .env
 }
-
-console.log(`Loaded env file ${envFile}`);
